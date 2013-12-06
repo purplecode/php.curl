8b2f3b2789c55fd9ad631924636f84633f2dd1a9
purplecode <niespammnie@gmail.com>
2013-12-06 18:05:25 +0100

    ssl verification tests

--- test/PCurlTest.php
+++ test/PCurlTest.php
@@ -6,15 +6,42 @@ use PurpleCode\PCurl;
 
 class PCurlTest extends PHPUnit_Framework_TestCase {
 
-    public function testShouldGetGooglePage() {
+	private function getCACertBundlePath() {
+		return __DIR__.'\ca-cert.crt';
+	}
+
+    public function testShouldGetGooglePageViaHttp() {
         // given
-         $cut = new PCurl('https://www.google.com');
+         $cut = new PCurl('http://www.google.pl');
 
         // when
         $response = $cut->get('/');
 
         // then
         $this->assertSelectRegExp('title', '/Google/', 1, $response);
+  }
+
+	public function testShouldGetGooglePageAndCheckHttpsCertificate() {
+        // given
+         $cut = new PCurl('https://www.google.pl');
+         $cut->useSSLCertificate($this->getCACertBundlePath());
+
+        // when
+        $response = $cut->get('/');
+
+        // then
+        $this->assertSelectRegExp('title', '/Google/', 1, $response);
+  	}
+
+    public function testShouldGetGooglePageAndThrowExceptionOnMissingCertificate() {
+        // given
+         $cut = new PCurl('https://www.google.com');
+
+         // then
+         $this->setExpectedException('PurpleCode\PCurlException');
+
+        // when
+        $response = $cut->get('/');
   }
 
 }
